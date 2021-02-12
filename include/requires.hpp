0fef5b93465385118df053a4a3f9432ea540ce54
condy <condy0919@gmail.com>
2021-02-12 21:16:43 +0800

    Add REQUIRES facility macro

---
+++ include/requires.hpp
@@ -0,0 +1,11 @@
+//! The REQUIRES facility macro
+//! SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef YAPDF_REQUIRES_HPP_
+#define YAPDF_REQUIRES_HPP_
+
+#include <type_traits>
+
+#define YAPDF_REQUIRES(...) std::enable_if_t<static_cast<bool>(__VA_ARGS__), int> = __LINE__
+
+#endif // YAPDF_REQUIRES_HPP_
