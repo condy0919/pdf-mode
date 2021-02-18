8353a24809cf4a9fcf224a5be1bb3a12a01d79fa
condy <condy0919@gmail.com>
2021-02-18 10:23:12 +0800

    Add regular void type

---
+++ include/void.hpp
@@ -0,0 +1,17 @@
+//! The regular `void` type.
+//!
+//! It's more like the unit type in functional programming language. Introduce here for facilitating template
+//! meta-programming.
+//!
+//! Visit http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2016/p0146r1.html for more information.
+
+#ifndef YAPDF_VOID_HPP_
+#define YAPDF_VOID_HPP_
+
+#include <variant>
+
+namespace yapdf {
+using Void = std::monostate;
+} // namespace yapdf
+
+#endif // YAPDF_VOID_HPP_
